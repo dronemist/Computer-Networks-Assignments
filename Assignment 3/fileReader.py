4af363b99c92da5a261c1890ff44abfd93bd68c8
Overlord <overlord@ubuntu.ubuntu-domain>
2019-10-12 02:52:19 +0530

    Data till 8 obtained, plotting remaining

--- Assignment 3/fileReader.py
+++ Assignment 3/fileReader.py
@@ -6,6 +6,9 @@ import numpy as np
 # Packet represented as array of dictionaries
 parsedPackets = []
 
+#Set of Server and client IPs, whether server or client recognised by seeing SYN packets
+serverIPs = set()
+clientIPs = set()
 
 def plot_bar_x(label, TCPflows):
   # this is for plotting purpose
@@ -23,8 +26,6 @@ def plot_bar_x(label, TCPflows):
 # NOTE: counted send and receive within the same TCP flow 
 
 def fileReader(fileName):
-  serverIPs = set()
-  clientIPs = set()
   TCPflows = set()
   TCPflowsForGraph = [set() for _ in range(24)]
   label = [i for i in range(24)]
@@ -32,6 +33,9 @@ def fileReader(fileName):
   startTime = 0.0
 
   global parsedPackets
+  global serverIPs
+  global clientIPs
+
   ''' Parses the input csv file '''
   with open(fileName) as fileIn:
     fileIn.readline()
@@ -81,6 +85,8 @@ def fileReader(fileName):
 
 def plotConnectionDurationCDF():
   global parsedPackets
+  global serverIPs
+  global clientIPs
 
   # Dictionary of ((sourceIP + sourcePort + destinationIP + destinationPort) strings, start times) of flows which have been SYNned
   TCPFlowsStartedInTime = {}
@@ -96,17 +102,52 @@ def plotConnectionDurationCDF():
   # Dictionary of (string, float) denoting the TCPFLow and its duration, here always client IP will come first in string
   TCPFlowConnectionDuration = {}
 
+  ''' For part 6 '''
+  # List of floats containing list of inter arrival times of SYN packets for new connections 
+  interArrivalOpeningTimeList = []
+  #Arrival Time of most recent SYN packet
+  mostRecentSYNPacketArrivalTime = 0
+
+  ''' For part 7 '''
+  #Inter arrival time for incoming packets to server
+  interArrivalIncomingPacketToServerTimeList = []
+  #Arrival time of most recent packet to server
+  mostRecentServerPacketArrivalTime = 0
+
+  ''' For part 8 '''
+  # List of incoming packet lengths to server
+  incomingPacketLengthList = []
+  # List of outgoing packet lengths to client
+  outgoingPacketLengthList = []
+
   for parsedPacket in parsedPackets:
-    print(parsedPacket)
+    # print(parsedPacket)
+    
+    info = parsedPacket["Info"].strip()
+    sourceIP = parsedPacket["Source"]
+    destinationIP = parsedPacket["Destination"]
+    timeOfCurrentPacketCapture = parsedPacket["Time"]
+    packetLength = parsedPacket["Length"]
+    currentArrivalTime = parsedPacket["Time"]
+
+    # if packet is incoming to server
+    if {destinationIP}.issubset(serverIPs):
+      currentServerPacketInterArrivalTime = currentArrivalTime - mostRecentServerPacketArrivalTime
+      interArrivalIncomingPacketToServerTimeList.append(currentServerPacketInterArrivalTime)
+      mostRecentServerPacketArrivalTime = currentArrivalTime
+
+      #Updating incoming packet length list
+      incomingPacketLengthList.append(packetLength)
+
+    # if packet is outgoing to client
+    if {destinationIP}.issubset(clientIPs):
+      #Updating outgoing packet length list
+      outgoingPacketLengthList.append(packetLength)
+
     if parsedPacket["Protocol"] == "TCP":
-      info = parsedPacket["Info"].strip()
       subWords = info.split()
       sourcePort = subWords[0]
       destinationPort = subWords[2]
-      sourceIP = parsedPacket["Source"]
-      destinationIP = parsedPacket["Destination"]
-      timeOfCurrentPacketCapture = parsedPacket["Time"]
-      packetLength = parsedPacket["Length"]
       flow = sourceIP + " " + sourcePort + " " + destinationIP + " " + destinationPort
       reverseFlow = destinationIP + " " + destinationPort + " " + sourceIP + " " + sourcePort
 
@@ -116,7 +157,13 @@ def plotConnectionDurationCDF():
         TCPNumBytesSentOverConnection[flow] = 0
         TCPNumBytesReceivedOverConnection[flow] = 0  
 
-        TCPFlowsStartedInTime[flow] = parsedPacket["Time"]
+        #Updating the Connection start time
+        TCPFlowsStartedInTime[flow] = currentArrivalTime
+
+        #Updating the inter arrival times
+        currentSYNInterArrivalTime = currentArrivalTime - mostRecentSYNPacketArrivalTime
+        interArrivalOpeningTimeList.append(currentSYNInterArrivalTime)
+        mostRecentSYNPacketArrivalTime = currentArrivalTime
 
       if "FIN" in subWords[3] or "RST" in subWords[3]:
         flowStartTime = TCPFlowsStartedInTime.get(flow)
@@ -140,12 +187,19 @@ def plotConnectionDurationCDF():
       if reverseFlowStartTime is not None:
         #Means packet sent from server to client
         TCPNumBytesReceivedOverConnection[reverseFlow] = TCPNumBytesReceivedOverConnection[reverseFlow] + packetLength
-      
+
+  # For plotting CDF of connection time and bytes sent      
   maxConnectionDuration = 0
 
   for (_, flowDuration) in TCPFlowConnectionDuration.items():
     maxConnectionDuration = max(maxConnectionDuration, flowDuration)
 
+
+  # For plotting CDF of inter arrival opening times
+  
+
+
+  # 
 
   # TODO: Everything obtained, plotting remaining
 
