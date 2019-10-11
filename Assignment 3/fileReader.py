fe91daabe1bbb419c8185e34bbfff170906f7509
Overlord <overlord@ubuntu.ubuntu-domain>
2019-10-11 21:17:16 +0530

    4 AND 5 DONE, plotting remaining

--- Assignment 3/fileReader.py
+++ Assignment 3/fileReader.py
@@ -3,6 +3,10 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Packet represented as array of dictionaries
+parsedPackets = []
+
+
 def plot_bar_x(label, TCPflows):
   # this is for plotting purpose
   index = np.arange(len(label))
@@ -26,6 +30,8 @@ def fileReader(fileName):
   label = [i for i in range(24)]
   # startTime denotes the time at which hour starts
   startTime = 0.0
+
+  global parsedPackets
   ''' Parses the input csv file '''
   with open(fileName) as fileIn:
     fileIn.readline()
@@ -41,6 +47,15 @@ def fileReader(fileName):
       info = words[6].strip('\"')
       if(len(words) > 7):
         info += words[7].strip('\"')
+      parsedPacket = {}
+      parsedPacket["No."] = int(packetNumber)
+      parsedPacket["Time"] = float(timeOfPacketCapture)
+      parsedPacket["Source"] = sourceIP
+      parsedPacket["Destination"] = destinationIP
+      parsedPacket["Protocol"] = protocol
+      parsedPacket["Length"] = int(packetLength)
+      parsedPacket["Info"] = info
+      parsedPackets.append(parsedPacket)
       # If protocol is TCP
       if protocol == "TCP":
         info = info.strip()
@@ -58,9 +73,82 @@ def fileReader(fileName):
             startTime += 60 * 60
           TCPflowsForGraph[int(startTime / (60 * 60))] = flow
   plot_bar_x(label,TCPflowsForGraph)
-  print(len(serverIPs)) 
-  print(len(clientIPs))      
-  print(len(TCPflows))
+  plotConnectionDurationCDF()
+  # print(len(serverIPs)) 
+  # print(len(clientIPs))      
+  # print(len(TCPflows))
+
+
+def plotConnectionDurationCDF():
+  global parsedPackets
+
+  # Dictionary of ((sourceIP + sourcePort + destinationIP + destinationPort) strings, start times) of flows which have been SYNned
+  TCPFlowsStartedInTime = {}
+
+  #NOTE: Bytes sent and received with respect to client
+
+  # Dictionary of ((sourceIP + sourcePort + destinationIP + destinationPort) strings, bytes sent)
+  TCPNumBytesSentOverConnection = {}
+
+  # Dictionary of ((sourceIP + sourcePort + destinationIP + destinationPort) strings, bytes received)
+  TCPNumBytesReceivedOverConnection = {}
+
+  # Dictionary of (string, float) denoting the TCPFLow and its duration, here always client IP will come first in string
+  TCPFlowConnectionDuration = {}
+
+  for parsedPacket in parsedPackets:
+    print(parsedPacket)
+    if parsedPacket["Protocol"] == "TCP":
+      info = parsedPacket["Info"].strip()
+      subWords = info.split()
+      sourcePort = subWords[0]
+      destinationPort = subWords[2]
+      sourceIP = parsedPacket["Source"]
+      destinationIP = parsedPacket["Destination"]
+      timeOfCurrentPacketCapture = parsedPacket["Time"]
+      packetLength = parsedPacket["Length"]
+      flow = sourceIP + " " + sourcePort + " " + destinationIP + " " + destinationPort
+      reverseFlow = destinationIP + " " + destinationPort + " " + sourceIP + " " + sourcePort
+
+      if "SYN" in subWords[3] and "ACK" not in subWords[4]:
+        
+        #Reset the connection
+        TCPNumBytesSentOverConnection[flow] = 0
+        TCPNumBytesReceivedOverConnection[flow] = 0  
+
+        TCPFlowsStartedInTime[flow] = parsedPacket["Time"]
+
+      if "FIN" in subWords[3] or "RST" in subWords[3]:
+        flowStartTime = TCPFlowsStartedInTime.get(flow)
+        reverseFlowStartTime = TCPFlowsStartedInTime.get(reverseFlow)
+
+        #Connection ended by client
+        if flowStartTime is not None:
+          TCPFlowConnectionDuration[flow] = timeOfCurrentPacketCapture - flowStartTime 
+
+        #Connection ended by host
+        if reverseFlowStartTime is not None:
+          TCPFlowConnectionDuration[reverseFlow] = timeOfCurrentPacketCapture - reverseFlowStartTime
+
+      flowStartTime = TCPFlowsStartedInTime.get(flow)
+      reverseFlowStartTime = TCPFlowsStartedInTime.get(reverseFlow)
+
+      if flowStartTime is not None:
+        #Means packet sent from client to server
+        TCPNumBytesSentOverConnection[flow] = TCPNumBytesSentOverConnection[flow] + packetLength
+
+      if reverseFlowStartTime is not None:
+        #Means packet sent from server to client
+        TCPNumBytesReceivedOverConnection[reverseFlow] = TCPNumBytesReceivedOverConnection[reverseFlow] + packetLength
+      
+  maxConnectionDuration = 0
+
+  for (_, flowDuration) in TCPFlowConnectionDuration.items():
+    maxConnectionDuration = max(maxConnectionDuration, flowDuration)
+
+
+  # TODO: Everything obtained, plotting remaining
 
 if __name__ == "__main__":
-    fileReader("1.csv")
+    traceFolderName = "Packet_traces"
+    fileReader(traceFolderName + "/1.csv")
